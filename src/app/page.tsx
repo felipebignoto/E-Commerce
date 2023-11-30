fd6204f9f998723c13aaee4a73f0f2553cca2578
Felipe-Bignoto <felipebignoto@gmail.com>
2023-11-30 18:21:24 -0300

    componente produto finalizado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { ProductType } from '@/types/ProductType'
+import Product from './components/Product'
+
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+export default async function Home() {
+  const products = await getProducts()
   return (
-    <div>
-      <h1>E-commerce</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        {products.map((product: ProductType) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
