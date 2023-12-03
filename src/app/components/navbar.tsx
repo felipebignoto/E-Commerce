6c662e069f26075a740553bd7a1e3f2291efc192
Felipe-Bignoto <felipebignoto@gmail.com>
2023-12-02 21:14:06 -0300

    adicionando clerk

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 
 export default function Navbar() {
   return (
@@ -9,6 +10,18 @@ export default function Navbar() {
       >
         Next Store
       </Link>
+      <div className="flex items-center gap-8">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <button className="uppercase rounded-md border border-gray-400 px-3 py-2">
+              Login
+            </button>
+          </SignInButton>
+        </SignedOut>
+      </div>
     </nav>
   )
 }
