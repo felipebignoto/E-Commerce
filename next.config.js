bacdf12e302d46123f0fce89a6c605e7bcbefa3e
Felipe-Bignoto <felipebignoto@gmail.com>
2023-12-27 16:56:06 -0300

    arrumar loading

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ['fakestoreapi.com'] },
+  images: { domains: ['files.stripe.com'] },
 }
 
 module.exports = nextConfig
