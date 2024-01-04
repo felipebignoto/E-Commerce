ee7ea35bb0f8af2a90b86bbaec510a6c792ebf3f
Felipe-Bignoto <felipebignoto@gmail.com>
2024-01-04 18:57:03 -0300

    formatar carrinho de compras

--- src/store.ts
+++ src/store.ts
@@ -4,8 +4,8 @@ import { ProductType } from './types/ProductType'
 
 type CartState = {
   cart: ProductType[]
-  // addToCart: (product: ProductType) => void
-  // removeFromCart: (product: string) => void
+  addProduct: (product: ProductType) => void
+  removeProduct: (product: ProductType) => void
   isOpen: boolean
   toggleCart: () => void
 }
@@ -14,6 +14,39 @@ export const useCartStore = create<CartState>()(
   persist(
     (set) => ({
       cart: [],
+      addProduct: (item) =>
+        set((state) => {
+          const product = state.cart.find((p) => p.id === item.id)
+
+          if (product) {
+            const updatedCart = state.cart.map((p) => {
+              if (p.id === item.id) {
+                return { ...p, quantity: p.quantity ? p.quantity + 1 : 1 }
+              }
+              return p
+            })
+            return { cart: updatedCart }
+          } else {
+            return { cart: [...state.cart, { ...item, quantity: 1 }] }
+          }
+        }),
+      removeProduct: (item) =>
+        set((state) => {
+          const existingProduct = state.cart.find((p) => p.id === item.id)
+
+          if (existingProduct && existingProduct.quantity! > 1) {
+            const updatedCart = state.cart.map((p) => {
+              if (p.id === item.id) {
+                return { ...p, quantity: p.quantity! - 1 }
+              }
+              return p
+            })
+            return { cart: updatedCart }
+          } else {
+            const filterdCart = state.cart.filter((p) => p.id !== item.id)
+            return { cart: filterdCart }
+          }
+        }),
       isOpen: false,
       toggleCart: () => set((state) => ({ isOpen: !state.isOpen })),
     }),
